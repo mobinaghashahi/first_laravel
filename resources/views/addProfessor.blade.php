0d59626bb0c9bd52ac86bdba73d1745da78dd55d
Mobin <mobinaghashahi13@gmail.com>
2022-01-29 22:31:04 +0330

    add AddProfessor page

---
+++ resources/views/addProfessor.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.master')
+@section('content')
+    <form method="post" name="register" action="professorChange.php" enctype="multipart/form-data" style="direction: rtl;text-align: right">
+        <div class="col-12">
+            <div class="col-12">
+                نام
+            </div>
+            <div class="col-5" style="float: right">
+                <input class="input" name="name" type="text" value="">
+            </div>
+            <div class="col-12">
+                نام خانوادگی
+            </div>
+            <div class="col-5" style="float: right">
+                <input class="input" name="family" type="text" value="">
+            </div>
+            <div class="col-12">
+                رزومه
+            </div>
+            <div class="col-5" style="float: right">
+                <input class="input" name="cv" type="text" value="">
+            </div>
+            <div class="col-12">
+                مدرک
+            </div>
+            <div class="col-5" style="float: right">
+                <input class="input" name="evidence" type="text" value="">
+            </div>
+            <div class="col-12">
+                ایمیل
+            </div>
+            <div class="col-5" style="float: right">
+                <input class="input" name="email" type="email" value="">
+            </div>
+            <div class="col-12">
+                شماره موبایل
+            </div>
+            <div class="col-5" style="float: right">
+                <input class="input" name="phone" type="text" value="">
+            </div>
+            <div class="col-12">
+                Select image to upload:
+
+                <input type="file" name="fileToUpload" id="fileToUpload">
+            </div>
+            <div class="col-12">
+                <input class="submit" name="addProfessor" type="submit" value="اضاف کردن">
+            </div>
+        </div>
+    </form>
+    </div>
+@endsection
